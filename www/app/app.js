d4871c75159708c6a8f37b7bcf024e7dd3c5de43
Patrick Sullivan <patdsully88@gmail.com>
2014-10-14 20:48:30 -0500

    first commit

---
+++ www/app/app.js
@@ -0,0 +1,55 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('app', ['ionic', 'app.controllers'])
+
+    .run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if(window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if(window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    })
+
+
+
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+
+            .state('app', {
+                url: "/app",
+                abstract: true,
+                templateUrl: "app/menu/menu.html",
+                controller: 'menuController'
+            })
+            .state('app.browse', {
+                url: "/browse",
+                views: {
+                    'menuContent' :{
+                        templateUrl: "app/accounts/browse.html"
+                    }
+                },
+                controller: 'accountsController'
+            })
+            .state('app.accountTransactions', {
+                url: "/accountTransactions/:name",
+                views: {
+                    'menuContent': {
+                        templateUrl: "app/accounts/accountTransactions.html"
+                    }
+                },
+                controller: 'accountsController'
+            });
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/browse');
+    });
---
+++ www/app/menu/menuController.js
@@ -0,0 +1,118 @@
+angular.module('app.controllers', [])
+
+    .controller('menuController', function($scope, $ionicModal, $timeout) {
+        // Form data for the login modal
+        $scope.loginData = {};
+
+        // Create the login modal that we will use later
+        $ionicModal.fromTemplateUrl('app/login/login.html', {
+            scope: $scope
+        }).then(function(modal) {
+            $scope.modal = modal;
+        });
+
+        // Triggered in the login modal to close it
+        $scope.closeLogin = function() {
+            $scope.modal.hide();
+        };
+
+        // Open the login modal
+        $scope.login = function() {
+            $scope.modal.show();
+        };
+
+        // Perform the login action when the user submits the login form
+        $scope.doLogin = function() {
+            console.log('Doing login', $scope.loginData);
+
+            // Simulate a login delay. Remove this and replace with your login
+            // code if using a login system
+            $timeout(function() {
+                $scope.closeLogin();
+            }, 1000);
+        };
+
+    })
+
+    .controller('accountsController', function($scope, $ionicModal, $stateParams, $timeout, $state) {
+
+        $scope.TestTitle = "asdfaf a";
+        $scope.addAccountData = {};
+
+        $scope.accounts = [];
+//        [
+//            {
+//                name: "ssSavings"
+//                , ammount : "1.00"
+//            },
+//            {
+//                name: "ssChecking"
+//                , ammount : "1.00"
+//            }
+//        ];
+
+        $scope.loadAccounts = function(){
+            var accountsstring = window.localStorage['accounts'];
+            if(accountsstring) {
+                $scope.accounts = angular.fromJson(accountsstring);
+            }
+
+            $scope.saveAccountsData();
+        };
+
+        $ionicModal.fromTemplateUrl('app/accounts/accountDetail.html', {
+            scope: $scope
+        }).then(function(acdmodal) {
+            $scope.accountDetailModal = acdmodal;
+        });
+
+        // Triggered in the login modal to close it
+        $scope.closeAccountDetail = function() {
+            $scope.accountDetailModal.hide();
+        };
+
+        // Open the login modal
+        $scope.addAccount = function() {
+            $scope.accountDetailModal.show();
+            $scope.addAccountData.ammount = "0.00";
+            $scope.addAccountData.name = "New Account";
+        };
+
+        $scope.loadAddAccount = function() {
+        };
+
+        // Perform the login action when the user submits the login form
+        $scope.doAddAccount = function() {
+            console.log('Doing Add', $scope.addAccountData.ammount);
+
+            // Simulate a login delay. Remove this and replace with your login
+            // code if using a login system
+            $timeout(function() {
+                $scope.closeAccountDetail();
+                $scope.accounts.push($scope.addAccountData);
+                $scope.addAccountData = {};
+                $scope.saveAccountsData();
+            }, 1000);
+        };
+
+        $scope.saveAccountsData = function(){
+            window.localStorage['accounts'] = angular.toJson($scope.accounts);
+        };
+
+
+        $scope.listCanSwipe = true;
+        $scope.accountClick = function(item){
+            console.log('nav ', item.name);
+            $state.go('app.accountTransactions');
+        };
+
+        $scope.onAccountDelete = function(item){
+            var index = $scope.accounts.indexOf(item);
+            $scope.accounts.splice(index, 1);
+            $scope.saveAccountsData();
+            $scope.$apply();
+            swal("All Done!", "Account is deleted!", "success")
+
+        };
+
+    });
