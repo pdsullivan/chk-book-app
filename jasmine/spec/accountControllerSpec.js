d561863e816956afb3c2ed4429f12684172f9d94
Patrick Sullivan <patdsully88@gmail.com>
2014-11-24 17:54:23 -0600

    wrote first tests with jasmine.
    need to flush out. but the structure is in and ready for coding… thank
    ya jesus.

---
+++ jasmine/spec/accountControllerSpec.js
@@ -0,0 +1,46 @@
+/**
+ * Created by patricksullivan on 11/24/14.
+ */
+
+describe("accountsController", function() {
+
+
+    var scope, controller;
+    beforeEach(module('app'));
+
+    beforeEach(inject(function ($rootScope, $controller, $ionicModal,$ionicPopup,$stateParams,$timeout,$state) {
+        scope = $rootScope.$new();
+
+            controller = $controller('accountsController', {
+                $scope: scope,
+                $ionicModal: $ionicModal,
+                $ionicPopup: $ionicPopup,
+                $stateParams: $stateParams,
+                $timeout: $timeout,
+                $state: $state
+
+        });
+    }));
+
+    it("should have a $scope variable", function() {
+        expect(scope).toBeDefined();
+    });
+
+    it("should have a accounts array", function(){
+        //test
+
+        expect(scope.accounts.length).toBe(0);
+    });
+
+    //
+    //it("should have a type", function(){
+    //
+    //
+    //});
+    //
+    //it("should have a starting value", function(){
+    //
+    //
+    //});
+
+});
