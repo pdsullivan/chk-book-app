72c4087f413ac626e1c54730b402df1c576e6db3
Patrick Sullivan <patdsully88@gmail.com>
2014-12-01 13:33:59 -0600

    forgot the karma js file.

--- karma.conf.js
+++ karma.conf.js
@@ -19,6 +19,8 @@ module.exports = function(config) {
       "www/lib/angular-mocks/angular-mocks.js",
       "www/app/app.js",
       "www/app/accounts/accountsController.js",
+      "www/app/dataServices/accountDataService.js",
+      "www/app/logging/logingService.js",
       "jasmine/spec/accountControllerSpec.js"
     ],
 
